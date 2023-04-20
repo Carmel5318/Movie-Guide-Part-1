6bd174afc2c17c47ef0ff08844e47628243e48c0
Carmel5318 <114884652+Carmel5318@users.noreply.github.com>
2023-04-20 10:33:01 -0400

    changes

--- Movie_Guide_Part_1.py
+++ Movie_Guide_Part_1.py
@@ -4,9 +4,7 @@
 
 
 def display_menu():
-    print('1. Add a movie')
-    print('2. View upcoming movies')
-    print('3. View all movies')
-    print('4. View movies by genre')
-    print('5. View movies by year')
-    print('6. Exit')
+    print("The Movie List Program")
+    print()
+    print("COMMAND MENU")
+   
