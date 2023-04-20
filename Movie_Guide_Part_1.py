611ab0f4b58936d04d0b3ad74354c20dbf492438
Carmel5318 <114884652+Carmel5318@users.noreply.github.com>
2023-04-20 10:15:43 -0400

    def display_menu():

--- Movie_Guide_Part_1.py
+++ Movie_Guide_Part_1.py
@@ -1,1 +1,12 @@
+#Niki Decker
+#CIS261
+#Movie Guide Part 1
 
+
+def display_menu():
+    print('1. Add a movie')
+    print('2. View upcoming movies')
+    print('3. View all movies')
+    print('4. View movies by genre')
+    print('5. View movies by year')
+    print('6. Exit')
